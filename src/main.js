53d0702e49daae448a3ac6f09dafd01dc1be64bd
Polkovn1k <wa0s@narod.ru>
2020-08-01 16:15:27 +0300

    render layout #1

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ const createInfoTemplate = () => {
       <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
       <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
     </div>
-  </section>`
+  </section>`;
 };
 
 const createCostTemplate = () => {
